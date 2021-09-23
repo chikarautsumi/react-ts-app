1670792080c9ccfcfd23f63f58ee81dd0a266870
Chikara Utsumi <chiksrskobu0225@gmail.com>
2021-09-23 22:02:06 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AddIcon from '@material-ui/icons/Add';
+import { Button } from '@material-ui/core';
+
+const App: React.FC = () => {
+  const [number, setNumber] = useState(0);
+  const addNumber = () => setNumber((i) => i + 1);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button variant="contained" color="primary" startIcon={<AddIcon />} onClick={addNumber}>
+          こんにちは {number}件
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
